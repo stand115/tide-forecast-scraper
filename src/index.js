de2c7c83ec08ed6351d586b882d77b24a065850d
Tyler Standley <tyler.standley21@gmail.com>
2018-06-27 21:40:22 -0700

    Logic created to determine when we're in daylight

--- src/index.js
+++ src/index.js
@@ -11,10 +11,13 @@ app.set("json spaces", 3);
 // we'll use the /scrape endpoint to trigger the web scraper
 app.get("/scrape", function(req, res) {
   let output = {};
+
+  // call the scrape function for each location and return the output object in response
   scrape("Half-Moon-Bay-California", output);
   scrape("Huntington-Beach", output);
   scrape("Providence-Rhode-Island", output);
   scrape("Wrightsville-Beach-North-Carolina", output);
+
   res.json(output);
 });
 
@@ -22,7 +25,9 @@ app.listen(port, () => {
   console.log("Listening on port " + port);
 });
 
+// helper function to make requests to forecast site and extract data
 let scrape = (location, output) => {
+  // initiale array to contain low tide date, format url
   output[location] = [];
   let url = `https://www.tide-forecast.com/locations/${location}/tides/latest`;
   let date = "";
@@ -30,8 +35,33 @@ let scrape = (location, output) => {
   request(url, function(error, response, html) {
     if (!error) {
       let $ = cheerio.load(html);
-      let inDayRange = false;
-      console.log($);
+      // use this to toggle when we're in and out of daylight
+      let daylight = false;
+
+      // data rows exist in the in the tbody element, that's what we'll traverse
+      $("tbody")
+        .children()
+        .each(function(i, el) {
+          // toggle daylight on when sunrise is found
+          if (
+            $(this)
+              .children()
+              .text()
+              .indexOf("Sunrise") > -1
+          ) {
+            daylight = true;
+          }
+
+          // toggle daylight off when sunset is found
+          if (
+            $(this)
+              .children()
+              .text()
+              .indexOf("Sunset") > -1
+          ) {
+            daylight = false;
+          }
+        });
     }
   });
 };
