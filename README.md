81478853b32f4c6f5a31cca54eecdfc3219335fc
Tyler Standley <tyler.standley21@gmail.com>
2018-06-27 21:29:50 -0700

    Extracting DOM for each location to be parsed, using Cheerio

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # tide-forecast-scraper
+
 A simple web scraper for checking tide pools
+
+## Description
+
+Using https://www.tide-forecast.com/, get tide forecasts for these locations:
+
+Half Moon Bay, California
+Huntington Beach, California
+Providence, Rhode Island
+Wrightsville Beach, North Carolina
+
+The forecast page for each location is loaded, and information on low tides (date, time, height) on low tides that occur after sunrise and before sunset is extracted.
+
+### Setup Environment
+
+```bash
+npm install
+```
+
+### Start the App
+
+```bash
+npm start
+```
+
+## Testing
+
+Visit http://localhost:4000/scrape in the browser to view the output in JSON format
--- src/index.js
+++ src/index.js
@@ -5,12 +5,35 @@ const cheerio = require("cheerio");
 const app = express();
 const port = 4000;
 
+// format json output for browser viewing
+app.set("json spaces", 3);
+
+// we'll use the /scrape endpoint to trigger the web scraper
 app.get("/scrape", function(req, res) {
-  res.end();
+  let output = {};
+  scrape("Half-Moon-Bay-California", output);
+  scrape("Huntington-Beach", output);
+  scrape("Providence-Rhode-Island", output);
+  scrape("Wrightsville-Beach-North-Carolina", output);
+  res.json(output);
 });
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+let scrape = (location, output) => {
+  output[location] = [];
+  let url = `https://www.tide-forecast.com/locations/${location}/tides/latest`;
+  let date = "";
+
+  request(url, function(error, response, html) {
+    if (!error) {
+      let $ = cheerio.load(html);
+      let inDayRange = false;
+      console.log($);
+    }
+  });
+};
 
 module.exports = app;
